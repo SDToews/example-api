d5eb1a3bfef39fa15aa457190952f19f93b7f347
Scott Toews <scott.toews@gov.bc.ca>
2022-06-29 10:23:33 -0700

    get customer id

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { CustomersService } from 'src/customers/services/customers/customers.service';
-
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { CustomersService } from '../../services/customers/customers.service';
+import { Request, Response } from 'express';
 @Controller('customers')
 export class CustomersController {
   constructor(private customerService: CustomersService) {}
 
-  @Get()
-  getCustomer() {
-    return this.customerService.findCustomer();
+  @Get(':id')
+  getCustomer(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customerService.findCustomerById(id);
+
+    if (customer) return customer;
+    else throw new HttpException('Customer not found', HttpStatus.NOT_FOUND);
   }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  private users = [
+    {
       id: 1,
       email: 'scott.toews@gmail.com',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: 'scott@gmail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'toews@gmail.com',
+      createdAt: new Date(),
+    },
+  ];
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
